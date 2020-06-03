aac5fe82c734cf8b5d51f333d3c84f813df40d36
chuansheng <2635906359@qq.com>
2020-06-03 09:34:47 +0800

    vue项目初始化, 集成element-ui

---
+++ src/components/layout/MyLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="MyLayout">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyLayout'
+  }
+</script>
+
+<style scoped>
+  .MyLayout{
+    width: 100%;
+    height: 100%;
+  }
+</style>
