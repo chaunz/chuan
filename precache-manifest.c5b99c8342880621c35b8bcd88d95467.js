1fe1b70bf85b16ee1f4b59edd605ee3c0b0ff965
chuansheng <2635906359@qq.com>
2020-06-16 10:15:32 +0800

    替换成react

---
+++ precache-manifest.c5b99c8342880621c35b8bcd88d95467.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078c03848a86e08615561068e63c6c81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "410b99309e327c01ce31",
+    "url": "/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "2b507d918b2f93e5c363",
+    "url": "/static/css/main.c6306b77.chunk.css"
+  },
+  {
+    "revision": "410b99309e327c01ce31",
+    "url": "/static/js/2.4503e8ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4503e8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b507d918b2f93e5c363",
+    "url": "/static/js/main.d90799b2.chunk.js"
+  },
+  {
+    "revision": "03e6e8d129bbaaa731eb",
+    "url": "/static/js/runtime-main.3d2e979e.js"
+  },
+  {
+    "revision": "99d51433077c23d79d52311f862d970e",
+    "url": "/static/media/bg-1.99d51433.jpg"
+  },
+  {
+    "revision": "7c20e4cc743870178a30ab93d1004237",
+    "url": "/static/media/bg-2.7c20e4cc.jpg"
+  },
+  {
+    "revision": "ef12d61f5645fe839c11d7847f6b8eb2",
+    "url": "/static/media/bg-3.ef12d61f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
